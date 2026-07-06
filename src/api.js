745bd2d3c10064ba2582bca91619f8b3df5a21bc
Aanya Jain <aanya31102007@gmail.com>
2026-07-06 20:28:06 +0530

    push

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API || "https://gstat-ai-backend.onrender.com/api",
 });
 
 export default api;
